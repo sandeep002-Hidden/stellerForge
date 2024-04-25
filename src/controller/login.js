2c7967d762d1d766debdb2dab5356b91e76b9756
sandeep002-Hidden <21cse382.sandeepmohapatra@giet.edu>
2024-04-25 19:36:16 +0530

    modified

--- src/controller/login.js
+++ src/controller/login.js
@@ -1,16 +1,24 @@
 import User from "../models/user.model.js";
 export default function handelLogin(req, res) {
-  const userName=req.body.dUsername
-  if(req.cookies.loggedInUser===userName){
-    return res.redirect("/user/profile")
-}
-  User.find({username:userName})
-    .then((user) =>{
-        if(req.body.dPassword===user[0].password){
-            const maxAge = 30 * 60 * 1000;
-            res.cookie('loggedInUser', userName, { maxAge, httpOnly: true });
-            res.redirect("/user/profile")
-        }
-    })
+  const userName = req.body.dUsername;
+  User.find({ username: userName })
+    .then((user) => {
+      if(user[0]!=null){
+      if (req.body.dPassword === user[0].password) {
+        const maxAge = 30 * 60 * 1000;
+        res.cookie("loggedInUser", userName, { maxAge, httpOnly: true });
+        res.redirect("/user/profile");
+      }
+      else{
+        res.render("login",{message:"Wrong Password"});
+      }
+    }
+    else{
+      res.render("login",{message:"User Does not exist"});
+    }
+  })
+
+ 
     .catch((err) => console.error("Error fetching users:", err));
+
 }
